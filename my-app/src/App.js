a5d5ec0e26967c9cc0b55b30099b32d500dac617
stefano-moioli <151653556+stefano-moioli@users.noreply.github.com>
2024-04-16 10:08:26 +0200

    navComponent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import NavComponent from "./components/navbar/NavComponent";
 
 
 function App() {
   return (
     <>
-    <div>prova</div>
-    
+    <NavComponent />
+
     </>
     
   );
---
+++ my-app/src/components/navbar/NavComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavComponent = () => {
+    return (
+        <nav>prova</nav>
+    )
+}
+
+export default NavComponent;
